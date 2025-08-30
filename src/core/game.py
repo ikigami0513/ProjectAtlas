ac60473c6151d9931071722fad65ad65be487373
ikigami0513 <ikigami0513@gmail.com>
2025-08-30 23:20:07 +0200

    added new animations and player movement

--- src/core/game.py
+++ src/core/game.py
@@ -44,11 +44,52 @@ class Game:
         self.atlas_manager.add("player/hair/hair_1/grey")
 
         self.animation_manager.add("player/base/idle/down", Animation(self.atlas_manager.get("player/base"), [f"idle_down_{i}" for i in range(6)]))
+        self.animation_manager.add("player/base/idle/right", Animation(self.atlas_manager.get("player/base"), [f"idle_right_{i}" for i in range(6)]))
+        self.animation_manager.add("player/base/idle/up", Animation(self.atlas_manager.get("player/base"), [f"idle_up_{i}" for i in range(6)]))
+
+        self.animation_manager.add("player/base/walk/down", Animation(self.atlas_manager.get("player/base"), [f"walk_down_{i}" for i in range(6)]))
+        self.animation_manager.add("player/base/walk/right", Animation(self.atlas_manager.get("player/base"), [f"walk_right_{i}" for i in range(6)]))
+        self.animation_manager.add("player/base/walk/up", Animation(self.atlas_manager.get("player/base"), [f"walk_up_{i}" for i in range(6)]))
+
         self.animation_manager.add("player/shoes/white/idle/down", Animation(self.atlas_manager.get("player/shoes/white"), [f"idle_down_{i}" for i in range(6)]))
+        self.animation_manager.add("player/shoes/white/idle/right", Animation(self.atlas_manager.get("player/shoes/white"), [f"idle_right_{i}" for i in range(6)]))
+        self.animation_manager.add("player/shoes/white/idle/up", Animation(self.atlas_manager.get("player/shoes/white"), [f"idle_up_{i}" for i in range(6)]))
+
+        self.animation_manager.add("player/shoes/white/walk/down", Animation(self.atlas_manager.get("player/shoes/white"), [f"walk_down_{i}" for i in range(6)]))
+        self.animation_manager.add("player/shoes/white/walk/right", Animation(self.atlas_manager.get("player/shoes/white"), [f"walk_right_{i}" for i in range(6)]))
+        self.animation_manager.add("player/shoes/white/walk/up", Animation(self.atlas_manager.get("player/shoes/white"), [f"walk_up_{i}" for i in range(6)]))
+
         self.animation_manager.add("player/pants/og/blue/idle/down", Animation(self.atlas_manager.get("player/pants/og/blue"), [f"idle_down_{i}" for i in range(6)]))
+        self.animation_manager.add("player/pants/og/blue/idle/right", Animation(self.atlas_manager.get("player/pants/og/blue"), [f"idle_right_{i}" for i in range(6)]))
+        self.animation_manager.add("player/pants/og/blue/idle/up", Animation(self.atlas_manager.get("player/pants/og/blue"), [f"idle_up_{i}" for i in range(6)]))
+
+        self.animation_manager.add("player/pants/og/blue/walk/down", Animation(self.atlas_manager.get("player/pants/og/blue"), [f"walk_down_{i}" for i in range(6)]))
+        self.animation_manager.add("player/pants/og/blue/walk/right", Animation(self.atlas_manager.get("player/pants/og/blue"), [f"walk_right_{i}" for i in range(6)]))
+        self.animation_manager.add("player/pants/og/blue/walk/up", Animation(self.atlas_manager.get("player/pants/og/blue"), [f"walk_up_{i}" for i in range(6)]))
+
         self.animation_manager.add("player/chest/og/black/idle/down", Animation(self.atlas_manager.get("player/chest/og/black"), [f"idle_down_{i}" for i in range(6)]))
+        self.animation_manager.add("player/chest/og/black/idle/right", Animation(self.atlas_manager.get("player/chest/og/black"), [f"idle_right_{i}" for i in range(6)]))
+        self.animation_manager.add("player/chest/og/black/idle/up", Animation(self.atlas_manager.get("player/chest/og/black"), [f"idle_up_{i}" for i in range(6)]))
+
+        self.animation_manager.add("player/chest/og/black/walk/down", Animation(self.atlas_manager.get("player/chest/og/black"), [f"walk_down_{i}" for i in range(6)]))
+        self.animation_manager.add("player/chest/og/black/walk/right", Animation(self.atlas_manager.get("player/chest/og/black"), [f"walk_right_{i}" for i in range(6)]))
+        self.animation_manager.add("player/chest/og/black/walk/up", Animation(self.atlas_manager.get("player/chest/og/black"), [f"walk_up_{i}" for i in range(6)]))
+
         self.animation_manager.add("player/hand/hand/idle/down", Animation(self.atlas_manager.get("player/hand/hand"), [f"idle_down_{i}" for i in range(6)]))
+        self.animation_manager.add("player/hand/hand/idle/right", Animation(self.atlas_manager.get("player/hand/hand"), [f"idle_right_{i}" for i in range(6)]))
+        self.animation_manager.add("player/hand/hand/idle/up", Animation(self.atlas_manager.get("player/hand/hand"), [f"idle_up_{i}" for i in range(6)]))
+
+        self.animation_manager.add("player/hand/hand/walk/down", Animation(self.atlas_manager.get("player/hand/hand"), [f"walk_down_{i}" for i in range(6)]))
+        self.animation_manager.add("player/hand/hand/walk/right", Animation(self.atlas_manager.get("player/hand/hand"), [f"walk_right_{i}" for i in range(6)]))
+        self.animation_manager.add("player/hand/hand/walk/up", Animation(self.atlas_manager.get("player/hand/hand"), [f"walk_up_{i}" for i in range(6)]))
+
         self.animation_manager.add("player/hair/hair_1/grey/idle/down", Animation(self.atlas_manager.get("player/hair/hair_1/grey"), [f"idle_down_{i}" for i in range(6)]))
+        self.animation_manager.add("player/hair/hair_1/grey/idle/right", Animation(self.atlas_manager.get("player/hair/hair_1/grey"), [f"idle_right_{i}" for i in range(6)]))
+        self.animation_manager.add("player/hair/hair_1/grey/idle/up", Animation(self.atlas_manager.get("player/hair/hair_1/grey"), [f"idle_up_{i}" for i in range(6)]))
+
+        self.animation_manager.add("player/hair/hair_1/grey/walk/down", Animation(self.atlas_manager.get("player/hair/hair_1/grey"), [f"walk_down_{i}" for i in range(6)]))
+        self.animation_manager.add("player/hair/hair_1/grey/walk/right", Animation(self.atlas_manager.get("player/hair/hair_1/grey"), [f"walk_right_{i}" for i in range(6)]))
+        self.animation_manager.add("player/hair/hair_1/grey/walk/up", Animation(self.atlas_manager.get("player/hair/hair_1/grey"), [f"walk_up_{i}" for i in range(6)]))
 
         self.animation_manager.add("cow_1_idle_left", Animation(self.atlas_manager.get("cow_1"), ["idle_left_0", "idle_left_1"]))
         self.animation_manager.add("cow_1_idle_down", Animation(self.atlas_manager.get("cow_1"), ["idle_down_0", "idle_down_1"]))
--- src/entities/player.py
+++ src/entities/player.py
@@ -1,19 +1,37 @@
 import pygame
+from enum import Enum
 from entities.entity import Entity
 from graphics.animation_manager import AnimationManager
 from graphics.animation import Animation
 from typing import Tuple, List
 
 
+class Direction(Enum):
+    DOWN = "down"
+    UP = "up"
+    LEFT = "left"
+    RIGHT = "right"
+
+
+class State(Enum):
+    IDLE = "idle"
+    WALK = "walk"
+
+
 class Player(Entity):
     def __init__(self, position: Tuple[float, float], groups: List[pygame.Surface]):
         self.animation_manager = AnimationManager()
-        super().__init__(position, self.animation_manager.get("player/base/idle/down"), groups)
-        self.shoes = self.animation_manager.get("player/shoes/white/idle/down")
-        self.pants = self.animation_manager.get("player/pants/og/blue/idle/down")
-        self.chest = self.animation_manager.get("player/chest/og/black/idle/down")
-        self.hand = self.animation_manager.get("player/hand/hand/idle/down")
-        self.hair = self.animation_manager.get("player/hair/hair_1/grey/idle/down")
+        self.direction = Direction.LEFT
+        self.state = State.IDLE
+        self.speed = 200
+        self.velocity = pygame.Vector2()
+
+        super().__init__(position, self.animation_manager.get(f"player/base/{self.state.value}/{self.get_animation_direction().value}"), groups)
+        self.shoes = self.animation_manager.get(f"player/shoes/white/{self.state.value}/{self.get_animation_direction().value}")
+        self.pants = self.animation_manager.get(f"player/pants/og/blue/{self.state.value}/{self.get_animation_direction().value}")
+        self.chest = self.animation_manager.get(f"player/chest/og/black/{self.state.value}/{self.get_animation_direction().value}")
+        self.hand = self.animation_manager.get(f"player/hand/hand/{self.state.value}/{self.get_animation_direction().value}")
+        self.hair = self.animation_manager.get(f"player/hair/hair_1/grey/{self.state.value}/{self.get_animation_direction().value}")
 
         self.parts: List[Animation] = [
             self.animation,  # Base
@@ -24,8 +42,23 @@ class Player(Entity):
             self.hair
         ]
 
+    def get_animation_direction(self) -> Direction:
+        if self.direction == Direction.LEFT:
+            return Direction.RIGHT
+        return self.direction
+
     def update(self, delta_time: float) -> None:
         self.input()
+
+        if self.velocity.magnitude() > 0:
+            self.state = State.WALK
+        else:
+            self.state = State.IDLE
+
+        self.set_animation()
+
+        self.rect.x += self.velocity.x * self.speed * delta_time
+        self.rect.y += self.velocity.y * self.speed * delta_time
 
         for animation in self.parts:
             animation.update(delta_time)
@@ -34,16 +67,25 @@ class Player(Entity):
 
     def input(self) -> None:
         keys = pygame.key.get_pressed()
+        self.velocity.x = 0
+        self.velocity.y = 0
 
         if keys[pygame.K_z]:
-            print("up")
+            self.velocity.y = -1
+            self.direction = Direction.UP
         elif keys[pygame.K_s]:
-            print("down")
+            self.velocity.y = 1
+            self.direction = Direction.DOWN
 
         if keys[pygame.K_q]:
-            print("left")
+            self.velocity.x = -1
+            self.direction = Direction.LEFT
         elif keys[pygame.K_d]:
-            print("right")
+            self.velocity.x = 1
+            self.direction = Direction.RIGHT
+
+        if self.velocity.magnitude() > 0:
+            self.velocity.normalize_ip()
 
     def get_image(self) -> pygame.Surface:
         base_image = self.animation.current_frame.copy()
@@ -53,4 +95,24 @@ class Player(Entity):
         base_image.blit(self.hand.current_frame.copy(), (0, 0))
         base_image.blit(self.hair.current_frame.copy(), (0, 0))
 
+        if self.direction == Direction.LEFT:
+            base_image = pygame.transform.flip(base_image, True, False)
+
         return base_image
+    
+    def set_animation(self) -> None:
+        self.animation = self.animation_manager.get(f"player/base/{self.state.value}/{self.get_animation_direction().value}")
+        self.shoes = self.animation_manager.get(f"player/shoes/white/{self.state.value}/{self.get_animation_direction().value}")
+        self.pants = self.animation_manager.get(f"player/pants/og/blue/{self.state.value}/{self.get_animation_direction().value}")
+        self.chest = self.animation_manager.get(f"player/chest/og/black/{self.state.value}/{self.get_animation_direction().value}")
+        self.hand = self.animation_manager.get(f"player/hand/hand/{self.state.value}/{self.get_animation_direction().value}")
+        self.hair = self.animation_manager.get(f"player/hair/hair_1/grey/{self.state.value}/{self.get_animation_direction().value}")
+
+        self.parts = [
+            self.animation, 
+            self.shoes,
+            self.pants,
+            self.chest,
+            self.hand,
+            self.hair
+        ]
